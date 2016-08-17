de11d364e5b7aa66e28f6054c120e9b102d29aa0
sunnyzhy <iamaant100@163.com>
2016-08-17 12:03:48 +0800

    WPF NoBorderForm

---
+++ NoBorderForm3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+    1、拖动带ScrollViewer的无边框窗体
+    2、点击空白区域关闭弹窗
+ */
+namespace NoBorderForm3
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollViewer_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window1 w = new Window1();
+            w.Show();
+        }
+    }
+}
